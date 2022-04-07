0ce6f0f98647b24356e4cb809ed8155e49ab6ad2
FabioLaForesta <fabiolaforesta@icloud.com>
2022-04-07 16:24:39 +0200

    Add Main - 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,29 +36,44 @@ const team = [{
 
 
 
-// 1. Stampare su console le informazioni di nome, ruolo e la stringa della foto.
+/* 1. Stampare su console le informazioni di nome, ruolo e la stringa della foto.
 for (let i = 0; i < team.length; i++) {
     const person = team[i];
 
-    //console.log(person); // Singole persone all'interno dell'array. Complete di tutte le info.
-    //console.log(person.name); //Singoli nomi delle persone all'interno dell'array.
-    //console.log(person.role); //Singoli ruoli delle persone all'interno dell'array.
-    //console.log(person.image); //Singole img delle persone all'interno dell'array.
+    console.log(person); // Singole persone all'interno dell'array. Complete di tutte le info.
+    console.log(person.name); //Singoli nomi delle persone all'interno dell'array.
+    console.log(person.role); //Singoli ruoli delle persone all'interno dell'array.
+    console.log(person.image); //Singole img delle persone all'interno dell'array.
 }
+*/
 
 
 
-// 2. Stampare le stesse informazioni su DOM sottoforma di stringhe.
+/* 2. Stampare le stesse informazioni su DOM sottoforma di stringhe.
 const unorderList = document.getElementById('user') //Richiamo il nodo principale della DOM.
 
 for (let i = 0; i < team.length; i++) {
     const person = team[i];
     //console.log(person); // Singole persone all'interno dell'array. Complete di tutte le info.
     const listItem = document.createElement('li'); //Creo i nodi della DOM in cui stampo i dati.
+    listItem.innerHTML += person.name + ", " + person.role + ", " + person.image //Aggiungo i dati (nome e ruolo).
+    unorderList.append(listItem); //Appendo i nodi nell'HTML.
+}
+*/
+
+
+const unorderList = document.getElementById('user') //Richiamo il nodo principale della DOM.
+
+// 3. Trasformare la stringa foto in una immagine effettiva.
+for (let i = 0; i < team.length; i++) {
+    const person = team[i];
+    //console.log(person); // Singole persone all'interno dell'array. Complete di tutte le info.
+    const listItem = document.createElement('li'); //Creo i nodi della DOM in cui stampo i dati.
+    const imgElement = document.createElement('img'); // Creo i sotto-nodi della DOM.
+    //console.log(imgElement);
+    imgElement.src = './img/' + person.image; //Aggiungo il percorso della cartella.
+    //console.log(imgElement);
+    listItem.append(imgElement); //Appendo img ai li.
     listItem.innerHTML += person.name + ", " + person.role //Aggiungo i dati (nome e ruolo).
     unorderList.append(listItem); //Appendo i nodi nell'HTML.
 }
-
-
-
-// 3. Trasformare la stringa foto in una immagine effettiva
